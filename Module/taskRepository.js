d2258f62d3e1eba60b191f5e9de2365d58cb621b
Patrick Cool <patrickscool@gmail.com>
2021-01-19 20:51:21 -0800

    made return statements have the methods

--- Module/taskRepository.js
+++ Module/taskRepository.js
@@ -1,11 +1,18 @@
 var repo = function () {
-    return {
-        get: function (id) {
-            console.info('Getting task ' + id);
-            return {
-                name: 'new task from db'
-            }
+    var db = {};
+
+    var get = function (id) {
+        console.info('Getting task ' + id);
+        return {
+            name: 'new task from db'
         }
+    }
+    var save = function (task) {
+        console.info('Save task ' + task.name);
+    }
+    return {
+        get: get,
+        save: save
     }
 }
 module.exports = repo();
